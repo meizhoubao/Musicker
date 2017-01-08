98ece6b243208b79796cb4786cf264351fe1e519
meizhoubao <18032773381@163.com>
2017-01-08 21:21:39 +0800

    Update source-format.py

---
+++ source-format.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+playlist_format = "http://music.163.com/api/playlist/detail?id=%s"
+
+song_format = "http://music.163.com/api/song/detail/?" +         \
+                                  "id=%s&ids=%%5B%s%%5D&csrf_token="
