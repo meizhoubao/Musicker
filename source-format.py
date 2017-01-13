cdcc6833e25959d4a45b76b0d522833f88008536
meizhoubao <18032773381@163.com>
2017-01-13 21:31:25 +0800

    Update source-format.py

--- source-format.py
+++ source-format.py
@@ -2,5 +2,5 @@
 
 playlist_format = "http://music.163.com/api/playlist/detail?id=%s"
 
-song_format = "http://music.163.com/api/song/detail/?" +         \
+single_format = "http://music.163.com/api/song/detail/?" +         \
                                   "id=%s&ids=%%5B%s%%5D&csrf_token="
