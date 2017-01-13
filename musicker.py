a4a3a806999723c5ed824d58d649b651354e9065
meizhoubao <18032773381@163.com>
2017-01-13 22:20:09 +0800

    Add partial functions

--- musicker.py
+++ musicker.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 from source-format import *
+import requests
+import argparse
+
+parse = argparse.ArgumentParser()
+parse.add_argument("id-number", type=int, help="id number")
+paese.add_argument("-p", "--playlist", action="store_true",
+                   help="id number is for playlist if specified \
+                   or for single if not")
+args = parse.parse_args()
+
+
+def download_playlist(url):
+    pass
+
+
+def download_single(url):
+    r = requests.get(url)
+    mp3_url = r.json()['song'][0]['mp3Url']
+
+
+if args.playlist:
+    playlist_url = playlist_format % args.id-number
+    download_playlist(playlist_url)
+else:
+    single_url = single_format % (args.id-number, args.id-number)
+    download_single(single_url)
