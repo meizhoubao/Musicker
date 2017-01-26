8ab58a68452c9a08c9462a8ac4e5df26223bb997
meizhoubao <18032773381@163.com>
2017-01-26 13:40:44 +0800

    Fixed several bugs and finish sing download

--- musicker.py
+++ musicker.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from source-format import *
+from source import *
 import requests
 import argparse
+import urllib
 
 parse = argparse.ArgumentParser()
-parse.add_argument("id-number", type=int, help="id number")
-paese.add_argument("-p", "--playlist", action="store_true",
+parse.add_argument("id_number", type=int, help="id number")
+parse.add_argument("-p", "--playlist", action="store_true",
                    help="id number is for playlist if specified \
                    or for single if not")
 args = parse.parse_args()
@@ -19,10 +20,13 @@ def download_single(url):
     r = requests.get(url)
     song_name = r.json()['songs'][0]['name']
     mp3_url = r.json()['songs'][0]['mp3Url']
+    print("Downloading......")
+    urllib.request.urlretrieve(mp3_url, song_name + '.mp3')
+    print("Complete!")
 
 if args.playlist:
-    playlist_url = playlist_format % args.id-number
+    playlist_url = playlist_format % args.id_number
     download_playlist(playlist_url)
 else:
-    single_url = single_format % (args.id-number, args.id-number)
+    single_url = single_format % (args.id_number, args.id_number)
     download_single(single_url)
